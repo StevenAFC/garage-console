8c9e569b7e6cc0238d42a6bcbbe41fa60801f0a3
Steven Beeching <steven.beeching@gmail.com>
2020-06-11 16:10:30 +0100

    Fix device activation date

--- src/AlarmDevice.js
+++ src/AlarmDevice.js
@@ -2,6 +2,16 @@ import React from "react";
 import moment from "moment";
 import { Card, Icon, Feed } from "semantic-ui-react";
 
+const convertDate = (date) => {
+  let d = moment.unix(date / 1000).fromNow();
+
+  if (d === "Invalid date") {
+    d = moment(date).fromNow();
+  }
+
+  return d;
+};
+
 const AlarmDevice = ({ device }) => {
   return (
     <Card width="200">
@@ -21,9 +31,7 @@ const AlarmDevice = ({ device }) => {
                   <Icon name="warning circle" color="grey" />
                 </Feed.Label>
                 <Feed.Content>
-                  <Feed.Date
-                    content={moment.unix(alert.createdAt / 1000).fromNow()}
-                  />
+                  <Feed.Date content={convertDate(alert.createdAt)} />
                   <Feed.Summary>Sensor Activated</Feed.Summary>
                 </Feed.Content>
               </Feed.Event>
