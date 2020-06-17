b74f7be397202964dc82079e99e6b7c4c50a1b77
Steven Beeching <steven.beeching@gmail.com>
2020-06-17 13:17:08 +0100

    Display error on temp chart

--- src/TemperatureChart.js
+++ src/TemperatureChart.js
@@ -22,7 +22,7 @@ const TemperatureChart = () => {
   });
 
   if (loading) return <Loader>Loading...</Loader>;
-  if (error) return <p>ERROR</p>;
+  if (error) return <p>{error}</p>;
   if (!data) return <p>Not found</p>;
 
   var hour = d3.timeFormat("%Y-%m-%dT%H:00:00.00Z");
