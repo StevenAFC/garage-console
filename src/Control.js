06381929c6e42c558f845a3d319c3a120a39948e
Steven Beeching <steven.beeching@gmail.com>
2020-06-17 17:16:43 +0100

    State is managed by server via subscription

--- src/Control.js
+++ src/Control.js
@@ -5,59 +5,38 @@ import gql from "graphql-tag";
 
 export const DEVICE_PULSE = gql`
   mutation DevicePulse($id: ID!) {
-    devicePulse(id: $id) {
-      state
-      duration
-    }
+    devicePulse(id: $id)
   }
 `;
 
-const Control = ({ device }) => {
+const Control = ({ deviceState: { device, state } }) => {
   const [disabled, setDisabled] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [to, setTo] = useState(null);
+
   return (
     <Mutation mutation={DEVICE_PULSE}>
-      {(devicePulse, { data }) => (
+      {(devicePulse) => (
         <div>
           <form
             onSubmit={(e) => {
               e.preventDefault();
               setDisabled(true);
-              devicePulse({ variables: { id: device.id } }).then((response) => {
-                if (
-                  response.data.devicePulse &&
-                  response.data.devicePulse.duration
-                ) {
-                  if (loading) {
-                    setLoading(false);
-                    clearTimeout(to);
-                  } else {
-                    setLoading(true);
-                    setTo(
-                      setTimeout(() => {
-                        setLoading(false);
-                      }, response.data.devicePulse.duration)
-                    );
-                  }
-                }
-              });
-
-              setDisabled(false);
+              devicePulse({ variables: { id: device.id } }).then(() =>
+                setDisabled(false)
+              );
             }}
           >
             <Button
-              color={device.color}
+              color={state.state ? device.color : null}
               icon
               fluid
               disabled={disabled}
               type="submit"
               labelPosition="left"
-              loading={loading}
               style={{ marginBottom: 5 }}
             >
               {device.icon ? <Icon name={device.icon} /> : null}
               {device.name}
+              {state.state ? " - On" : " - Off"}
             </Button>
           </form>
         </div>
--- src/ControlPanel.js
+++ src/ControlPanel.js
@@ -2,31 +2,61 @@ import React from "react";
 import Control from "./Control";
 import gql from "graphql-tag";
 import { Query } from "@apollo/react-components";
+import { useSubscription } from "@apollo/react-hooks";
 
-export const GET_OUTPUT_DEVICES = gql`
-  query {
-    outputDevices {
+const DEVICE_STATE = gql`
+  subscription {
+    deviceState {
       id
-      name
-      icon
-      color
+      state
+    }
+  }
+`;
+
+export const GET_DEVICES = gql`
+  query {
+    deviceStates {
+      device {
+        id
+        name
+        icon
+        color
+        input
+        alarmDevice
+      }
+      state {
+        id
+        state
+      }
     }
   }
 `;
 
 const ControlPanel = () => {
+  useSubscription(DEVICE_STATE);
+
   return (
     <div>
-      <Query query={GET_OUTPUT_DEVICES}>
+      <Query query={GET_DEVICES}>
         {({ loading, error, data }) => {
           if (loading) return "Loading...";
           if (error) return `Error! ${error.message}`;
           return (
             <div>
-              {data.outputDevices &&
-                data.outputDevices.map((device) => (
-                  <Control device={device} key={device.id} />
-                ))}
+              {data.deviceStates &&
+                data.deviceStates
+                  .filter(
+                    (deviceState) =>
+                      deviceState.device &&
+                      !deviceState.device.input &&
+                      !deviceState.device.alarmDevice
+                  )
+                  .map((deviceState) => (
+                    <Control
+                      deviceState={deviceState}
+                      key={deviceState.device.id}
+                    />
+                  ))}
             </div>
           );
         }}
