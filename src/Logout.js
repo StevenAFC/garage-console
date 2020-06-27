d431ffa88d49944d3a02b4ffc9d27fc3891718ea
Steven Beeching <steven.beeching@gmail.com>
2020-06-27 10:40:24 +0100

    Reset client store on logout

--- src/Logout.js
+++ src/Logout.js
@@ -1,8 +1,13 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
+import { useApolloClient } from "@apollo/react-hooks";
 
 const Logout = () => {
   let history = useHistory();
+
+  const client = useApolloClient();
+  client.resetStore();
+
   localStorage.removeItem("token");
   history.push("/login");
   return <p>Logout</p>;
